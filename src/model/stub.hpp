99699f3aa80e131f8259d39dc1c1cd5d5b03df74
Bartłomiej Burdukiewicz <bartlomiej.burdukiewicz@gmail.com>
2021-09-14 23:02:21 +0200

    stub: generate values using time as reference.
    Signed-off-by: Bartłomiej Burdukiewicz <bartlomiej.burdukiewicz@gmail.com>

--- src/model/stub.hpp
+++ src/model/stub.hpp
@@ -11,7 +11,7 @@ namespace provider {
 
 struct stub {
     constexpr auto value(const types::time_point t) noexcept -> types::currency {
-        return {std::abs(std::sin(m_sin_steps += 0.001f) * 1000.0f), t.point};
+        return {std::abs(std::sin((t.point % 1000) * 0.0001f) * 1000.0f), t.point};
     }
 
     constexpr auto range() const noexcept -> types::time_range {
@@ -20,9 +20,6 @@ struct stub {
 
     constexpr auto range_changed(std::function<void()> &&) noexcept {
     }
-
-private:
-    float m_sin_steps{};
 };
 
 constexpr auto iterate(provider::model auto model, std::function<void(const types::time_point, const types::currency value)> &&callable, const std::chrono::seconds resolution = 1s) noexcept {
