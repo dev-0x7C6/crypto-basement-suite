54415027fd2ec8c5d0e4c5b417eb9e0e2f57ed9c
Bartłomiej Burdukiewicz <bartlomiej.burdukiewicz@gmail.com>
2022-08-08 19:28:16 +0200

    cli: csv-converter: allow multiple inputs, align and fix data.
    Signed-off-by: Bartłomiej Burdukiewicz <bartlomiej.burdukiewicz@gmail.com>

--- src/cli/csv-converter/main.cpp
+++ src/cli/csv-converter/main.cpp
@@ -3,6 +3,7 @@
 #include <cstring>
 #include <fstream>
 #include <vector>
+#include <cmath>
 
 #include <range/v3/algorithm/sort.hpp>
 #include <range/v3/view/split.hpp>
@@ -14,6 +15,8 @@
 
 using namespace csv;
 
+using s32 = std::int32_t;
+using s64 = std::int64_t;
 using f32 = std::float_t;
 using u16 = std::uint16_t;
 using u32 = std::uint32_t;
@@ -34,6 +37,15 @@ struct sample {
     f32 close;
     volume vol;
     u32 trade_count;
+
+    constexpr auto operator<=>(const sample &) const noexcept = default;
+};
+
+struct statistics {
+    u64 empty{};
+    u64 missing{};
+    u64 unaligned{};
+    u64 clones{};
 };
 
 static_assert(sizeof(header) == 68);
@@ -62,47 +74,100 @@ auto read(const CSVRow &row) -> sample {
         .trade_count = get_u32(9)};
 }
 
+auto fix(const std::vector<sample> &v, statistics &stats) -> std::vector<sample> {
+    std::vector<sample> ret;
+    const auto diff = v[1].timestamp - v[0].timestamp;
+
+    s64 correction{};
+    spdlog::info("samples by: {}s", diff);
+
+    for (auto i = 0ull; i < v.size(); ++i) {
+        if (v[i].timestamp % diff) {
+            spdlog::warn("unaligned data at: {}, skipping", v[i].timestamp);
+            stats.unaligned++;
+            continue;
+        }
+
+        if (v[i].trade_count == 0) {
+            spdlog::warn("tradecount 0 at: {}, skipping", v[i].timestamp);
+            stats.empty++;
+        }
+
+        const s64 timestamp = v[i].timestamp - correction;
+        const s64 expected = diff * i + v.front().timestamp;
+        const auto skew = timestamp - expected;
+
+        if (skew) {
+            spdlog::warn("detected data skew at: {}, missing: {} samples", v[i].timestamp, std::abs(skew / diff));
+            stats.missing += std::abs(skew / diff);
+            for (auto j = expected; j < timestamp; j += diff) {
+                auto s = v[i];
+                s.timestamp = j;
+                s.trade_count = 0;
+                ret.emplace_back(s);
+            }
+            correction += skew;
+            continue;
+        }
+
+        ret.emplace_back(v[i]);
+    }
+
+    return ret;
+}
+
 auto main(int argc, char **argv) -> int {
     CLI::App app("CSV to binary converter for cryptodatadownload.com");
 
-    std::string input_file;
-    std::string output_dir{"./"};
+    std::vector<std::string> inputs;
+    std::string output_file;
 
-    auto input_file_opt = app.add_option("-i,--input,input", input_file, "CSV input file");
-    auto output_dir_opt = app.add_option("-o,--output,ouput", output_dir, "Output directory");
+    auto input_file_opt = app.add_option("-i,--input,input", inputs, "CSV input file");
+    auto output_dir_opt = app.add_option("-o,--output,ouput", output_file, "Output directory");
 
-    input_file_opt->required()->check(CLI::ExistingFile);
-    output_dir_opt->check(CLI::ExistingDirectory);
+    input_file_opt->allow_extra_args()->required()->check(CLI::ExistingFile);
+    output_dir_opt->required();
     CLI11_PARSE(app, argc, argv);
 
-    spdlog::info("input file: {}", input_file);
-    spdlog::info("output dir: {}", output_dir);
-
-    CSVReader reader(input_file);
-
     std::vector<sample> samples;
+    statistics stats{};
     header head{};
-    auto symbol = reader.begin()->operator[]("symbol").get<std::string>(); // assume same data
-    const auto size = symbol.size() > head.symbols.size() ? head.symbols.size() : symbol.size();
-    std::memcpy(head.symbols.data(), symbol.c_str(), size);
-    std::replace(symbol.begin(), symbol.end(), '/', '2');
 
-    std::ofstream output(output_dir + symbol + ".cmap", std::ios::out | std::ios::binary);
+    for (auto &&input : inputs) {
+        spdlog::info("parsing: {}", input);
+        CSVReader reader(input);
 
-    for (CSVRow &row : reader)
-        samples.emplace_back(read(row));
+        header head{};
+        auto symbol = reader.begin()->operator[]("symbol").get<std::string>(); // assume same data
+        const auto size = symbol.size() > head.symbols.size() ? head.symbols.size() : symbol.size();
+        std::memcpy(head.symbols.data(), symbol.c_str(), size);
+        std::replace(symbol.begin(), symbol.end(), '/', '2');
 
-    output.write(reinterpret_cast<const char *>(&head), sizeof(head));
+        for (CSVRow &row : reader)
+            samples.emplace_back(read(row));
+    }
+
+    std::ofstream output(output_file, std::ios::out | std::ios::binary);
 
     std::sort(samples.begin(), samples.end(), [](auto &&rhs, auto &&lhs) {
         return rhs.timestamp < lhs.timestamp;
     });
 
-    for (auto &&sample : samples)
-        output.write(reinterpret_cast<const char *>(&sample), sizeof(sample));
+    stats.clones = samples.size();
+    samples.erase(std::unique(samples.begin(), samples.end()), samples.end());
+    stats.clones = stats.clones - samples.size();
+
+    samples = fix(samples, stats);
+
+    output.write(reinterpret_cast<const char *>(&head), sizeof(head));
+    output.write(reinterpret_cast<const char *>(samples.data()), samples.size() * sizeof(sample));
 
     spdlog::info("samples count: {}", samples.size());
     spdlog::info("samples size: {:.2f}MiB", samples.size() * sizeof(sample) / 1024.0 / 1024.0);
+    spdlog::info("sample clone count: {}", stats.clones);
+    spdlog::info("sample empty count: {}", stats.empty);
+    spdlog::info("sample missing: {}", stats.missing);
+    spdlog::info("sample unaligned: {}", stats.unaligned);
 
     return 0;
 }
