35a1122a344d294f14e68676c2a91a25cee3dd19
Bartłomiej Burdukiewicz <bartlomiej.burdukiewicz@gmail.com>
2024-01-05 00:01:33 +0100

    libblockfrost: query multiple asset pages
    Signed-off-by: Bartłomiej Burdukiewicz <bartlomiej.burdukiewicz@gmail.com>

--- src/libblockfrost/private/src/balance.cpp
+++ src/libblockfrost/private/src/balance.cpp
@@ -23,18 +23,27 @@ auto blockfrost::v0::accounts_balance(const std::string &address, const options 
 }
 
 auto blockfrost::v0::accounts_assets_balance(const std::string &stake_key, const options &opts) -> std::vector<asset> {
-    const auto json = request(fmt::format("accounts/{}/addresses/assets", stake_key), opts);
-    if (!json) return {};
+    constexpr auto max_items_per_page = 100;
+    auto page{1};
 
-    std::vector<asset> ret;
-    for (auto &&entry : json.value()) {
-        const auto unit = entry.value("unit", "");
-        const auto quantity = entry.value("quantity", "0");
-        ret.emplace_back(asset{
-            .unit = unit,
-            .quantity = static_cast<double>(std::stoull(quantity)),
-        });
+    std::vector<asset> assets;
+
+    for (;; ++page) {
+        const auto json = request(fmt::format("accounts/{}/addresses/assets?count={}&page={}", stake_key, max_items_per_page, page), opts);
+        if (!json) return {};
+
+        for (auto &&entry : json.value()) {
+            const auto unit = entry.value("unit", "");
+            const auto quantity = entry.value("quantity", "0");
+            assets.emplace_back(asset{
+                .unit = unit,
+                .quantity = static_cast<double>(std::stoull(quantity)),
+            });
+        }
+
+        if (max_items_per_page != json.value().size())
+            break;
     }
 
-    return ret;
+    return assets;
 }
