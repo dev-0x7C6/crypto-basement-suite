9983df1f5989b6442a322b2b1ba3e70aa89a9911
Bartłomiej Burdukiewicz <bartlomiej.burdukiewicz@gmail.com>
2024-12-21 01:57:42 +0100

    pf: minor adjustments to share chart
    Signed-off-by: Bartłomiej Burdukiewicz <bartlomiej.burdukiewicz@gmail.com>

--- src/cli/portfolio/gui/share-chart.cpp
+++ src/cli/portfolio/gui/share-chart.cpp
@@ -3,31 +3,34 @@
 #include <QtCharts/QPieSeries>
 #include <QtCharts>
 
+#include <ranges>
+
 using namespace gui::chart;
 
 auto gui::chart::shares(const shares::shares &shares, const query_price_fn &query_price, double min, double max) -> QChartView * {
     auto series = new QPieSeries();
-    series->setHoleSize(0.2);
+    series->setHoleSize(0.5);
+    series->setPieSize(0.8);
+    series->setPieStartAngle(min * 2);
 
-    for (auto &&s : shares) {
+    for (auto &&s : shares | std::ranges::views::reverse) {
         if (s.share < min) continue;
         if (s.share > max) continue;
 
         auto slice = series->append(QString::fromStdString(query_price(s)), s.share);
-        slice->setLabelArmLengthFactor(0.3);
-
         series->setLabelsVisible(true);
         series->setLabelsPosition(QPieSlice::LabelOutside);
     }
 
     auto chart = new QChart();
     chart->setAnimationOptions(QChart::AllAnimations);
-    chart->setTheme(QChart::ChartThemeDark);
+    chart->setTheme(QChart::ChartThemeBlueCerulean);
     chart->legend()->setAlignment(Qt::AlignLeft);
+    chart->legend()->setReverseMarkers(true);
+    chart->setTitle("shares");
+    chart->addSeries(series);
 
     auto view = new QChartView(chart);
-    view->chart()->addSeries(series);
-
     view->setRenderHint(QPainter::Antialiasing);
     return view;
 };
